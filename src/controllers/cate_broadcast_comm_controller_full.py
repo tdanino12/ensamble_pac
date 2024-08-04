83421051bf5f09703ec8a9b523efa44ab0404029
tdanino12 <36441678+tdanino12@users.noreply.github.com>
2024-08-04 14:34:14 +0300

    Update cate_broadcast_comm_controller_full.py

--- src/controllers/cate_broadcast_comm_controller_full.py
+++ src/controllers/cate_broadcast_comm_controller_full.py
@@ -187,7 +187,10 @@ class CateBCommFMAC:
 			return agent_outs.view(ep_batch.batch_size, self.n_agents, -1)
 
 	def init_hidden(self, batch_size):
-		self.hidden_states = self.agent.init_hidden().unsqueeze(0).expand(batch_size, self.n_agents, -1)  # bav
+		self.hidden_states = self.agent.init_hidden()
+		for i in range(len(self.hidden_states)):
+		    self.hidden_states[i] = self.hidden_states[i].unsqueeze(0).expand(batch_size, self.n_agents, -1) 
+		#self.hidden_states = self.agent.init_hidden().unsqueeze(0).expand(batch_size, self.n_agents, -1)  # bav
 
 	def parameters(self):
 		return list(self.agent.parameters()) + list(self.comm.parameters())
